3b7d9f0ee92edc2467e0b3265e87ad936892e580
坂本 隆幸 <takayuki.sakamoto@daikin.co.jp>
2024-07-11 18:55:34 +0900

    ajaxコマンド PUTに変更

--- demo/views.py
+++ demo/views.py
@@ -2,11 +2,12 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, View
 from django.http.response import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import Favorite
 
 class IndexView(TemplateView):
     template_name = 'index.html';
-    
+   
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         
@@ -16,7 +17,7 @@ class IndexView(TemplateView):
         return context
 
 class UpdateFavoriteView(View):
-    def get(self, request, *args, **kwargs):
+    def put(self, request, *args, **kwargs):
         favo = Favorite.objects.get(pk=1)
         favo.favorite = not favo.favorite
         favo.save()
--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,52 @@
-const updateFavorite = async () => {
-    //  AJAX通信によりFavorite反転, 状態取得
-    let json_val = await fetch("http://127.0.0.1:8000/api/favorite")
-        .then((response)=> {
-            return ( response.json() );
-        })
-        .catch((error)=>{
-            console.log("通信失敗");
-        });
-
-    //  画像イメージ更新
-    let img_file = '/static/img/';
-
-    if ( json_val['favorite'] == true ) {
-        img_file += 'heart_on.png';
-    } else {
-        img_file += 'heart_off.png';
+const getCookie = (name) => {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
+    return cookieValue;
+}
 
-    document.getElementById('img_favo').src = img_file; 
+const csrfSafeMethod = (method) => {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+const updateFavorite = async () => {
+    let csrftoken = getCookie('csrftoken');
+           
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+            
+    //  AJAX通信によりFavorite反転, 状態取得
+    $.ajax({
+        type: 'PUT',
+        url: "http://127.0.0.1:8000/api/favorite/",
+    }).done((data) => {
+        //  画像イメージ更新
+        let img_file = '/static/img/';
+    
+        if ( data['favorite'] == true ) {
+            img_file += 'heart_on.png';
+        } else {
+            img_file += 'heart_off.png';
+        }
+        
+        document.getElementById('img_favo').src = img_file; 
+    }).fail((error) => {
+        alert('error!!!');
+    });
 }
